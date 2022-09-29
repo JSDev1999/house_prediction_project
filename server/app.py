34bb17fca008fe2f9d3b3a19ac17d60309825993
JSDEV1999 <rameshlearningadvance@gmail.com>
2022-09-29 21:01:12 +0530

    cleint updated

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import util
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/get_location_names', methods=['GET'])
@@ -9,22 +11,24 @@ def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+ #   response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    total_sqft = float(request.json['total_sqft'])
+    location = request.json['location']
+    bhk = int(request.json['bhk'])
+    bath = int(request.json['bath'])
+
+    #print("aaaa", total_sqft, location, bhk, bath)
 
     response = jsonify({
         'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+  #  response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
